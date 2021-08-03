ffbbadb20ff3256b3efb493ed6b6edabcd481cdc
J.Eduardo <eduardoczv@gmail.com>
2021-08-03 09:42:03 -0500

    handlres middleware

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js')
 
-const {logErrors, errorHander } = require('./utils/middleware/errorHandlers.js')
+const {logErrors, wrapError, errorHander } = require('./utils/middleware/errorHandlers.js');
+const notFoundHandler  = require('./utils/middleware/notFoundHandler.js');
 
 app.use(express.json());
+
+//routes
 moviesApi(app);
 
+//catch 404
+app.use(notFoundHandler);
+
+//validaciones
 app.use(logErrors);
+app.use(wrapError);
 app.use(errorHander);
 
 
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,8 +1,11 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 function withErrorStack(error, stack) {
+
+    console.log("config.dev",config.dev)
     if (config.dev) {
-        return { error, stack };
+        return { ...error, stack };
     }
 
     return error;
@@ -13,12 +16,22 @@ function logErrors(err, req, res, next) {
     next(err);
 }
 
+function wrapError(err,req,res,next){
+    if(err.isBoom){
+        next(boom.badImplementation(err));
+    }
+    next(err);
+}
+
 function errorHander(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json(withErrorStack(err.message, err.stack));
+    const { output: { statusCode, payload } } = err;
+
+    res.status(statusCode);
+    res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
     logErrors,
+    wrapError,
     errorHander
 }
